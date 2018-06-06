380004ba4d71ec2a7ce16d5f67b3d5a9dbf4348b
Danbot <dankovacek@gmail.com>
2018-06-05 20:56:29 -0700

    added cdn template tag loader for bokeh

---
+++ dkht/templatetags/bokeh.py
@@ -0,0 +1,17 @@
+from django import template
+from django.utils.safestring import mark_safe
+# from django.core.exceptions import PermissionDenied
+
+from bokeh.resources import CDN
+
+register = template.Library()
+
+
+@register.simple_tag()
+def bokeh_css():
+    return mark_safe(CDN.render_css())
+
+
+@register.simple_tag()
+def bokeh_js():
+    return mark_safe(CDN.render_js())
