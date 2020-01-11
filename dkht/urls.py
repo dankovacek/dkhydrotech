2dd23d9b4955286fc743addd8fc081a50644f328
Dan K <dankovacek@gmail.com>
2020-01-11 10:46:16 -0800

    remove bokeh script block from Entry view template

--- dkht/urls.py
+++ dkht/urls.py
@@ -42,4 +42,4 @@ urlpatterns = [
 #     path('dataviz/<viz_url>/', DataVizDetail.as_view(), name="dataviz-test"),
     path('sliders/', include('bkapps.urls')),
 
-] # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
