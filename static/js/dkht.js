13ddb3c0d25501598da213ce9c2c39a36dec21a3
danbot <dan@dkhydrotech.com>
2020-01-16 03:18:57 +0000

    add static folders to git track

---
+++ static/js/dkht.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+    console.log("Document ready.");
+    $(".dropdown-button").dropdown( {
+      hover: false,
+      constrainWidth: false
+    } );
+    $('.parallax').parallax();
+    $(".button-collapse").sideNav();
+    $('select').material_select();
+
+});
