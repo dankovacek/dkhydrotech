37ee7a48e0a37476c03e5e4f336fb4a19931252d
Dan K <dankovacek@gmail.com>
2020-01-15 23:45:44 -0800

    fix url for server_sessin

--- bkapps/urls.py
+++ bkapps/urls.py
@@ -6,5 +6,5 @@ app_name='bokeh_apps'
 
 urlpatterns = [
     # path('', views.sliders, name='sliders'),
-    path('/flood_msmt_error_simulator', views.FloodMsmtErrorSimulator.as_view(), name="flood_msmt_err_sim"),
+    path('flood_msmt_error_simulator', views.FloodMsmtErrorSimulator.as_view(), name="flood_msmt_err_sim"),
 ]
