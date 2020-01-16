fc0054b3c6252e07b3af874fd75207a6fdc54460
Dan K <dankovacek@gmail.com>
2020-01-15 21:05:54 -0800

    fix missing reference in views calling bokeh server document

--- bkapps/views.py
+++ bkapps/views.py
@@ -31,7 +31,8 @@ class DataVizDetail(TemplateView):
                 bk_url = 'http://127.0.0.1:5006' + app_ID
                 bk_script = server_document(url=bk_url)
             else:
-                bk_script = server_document(url=app_ID, relative_urls=True, 
+                bk_url = app_ID
+                bk_script = server_document(url=bk_url, relative_urls=True, 
                                             resources=None)
 
             logging.info('views url = {}'.format(bk_url))
