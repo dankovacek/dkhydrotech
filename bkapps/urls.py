026142ca1337e933b6c3085392ca47e4c5f55806
Dan K <dankovacek@gmail.com>
2020-01-16 15:59:30 -0800

    fix server_session params

--- bkapps/urls.py
+++ bkapps/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'bokeh_apps'
 
 urlpatterns = [
-    path('sliders', views.sliders_view, name='sliders'),
+    path('sliders', views.sliders_view.as_view(), name='sliders'),
     path('flood_msmt_error_simulaton', views.FloodMsmtErrorSimulator.as_view(), name="flood_msmt_err_sim"),
 ]
