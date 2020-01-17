171e0b8178a1f7ad0d65646badb360c6566e8744
Dan K <dankovacek@gmail.com>
2020-01-16 18:40:03 -0800

    fix app url namespacing for template links

--- bkapps/urls.py
+++ bkapps/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 
 from . import views
 
-app_name = 'bokeh_apps'
+app_name = 'bkapps'
 
 urlpatterns = [
-    path('<app_ID>', views.BokehView.as_view(), name='bokeh-views'),
+    path('flood_msmt', views.BokehView.as_view(), 
+    name='bokeh-flood-msmt', kwargs={'app_ID': 'flood_msmt'}),
+    path('sliders', views.BokehView.as_view(), 
+    name='bokeh-sliders', kwargs={'app_ID': 'sliders'})
 ]
