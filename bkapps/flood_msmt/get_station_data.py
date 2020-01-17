0e7c8dea1a59aeb6cc8338e923cdc1fe61c46bb7
Dan K <dankovacek@gmail.com>
2020-01-16 17:54:31 -0800

    update DB path

--- bkapps/flood_msmt/get_station_data.py
+++ bkapps/flood_msmt/get_station_data.py
@@ -26,7 +26,7 @@ except FileNotFoundError as e:
     print(e)
 
 try:
-    DB_DIR = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), 'db/')
+    DB_DIR = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), 'db/hydat_db/')
 except FileNotFoundError as e:
     logging.error('Debug=False in settings.')
     print(e)
