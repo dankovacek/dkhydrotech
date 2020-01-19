405dd5b0d933ed68449c3b269334b7b4ef8e1ab7
Dan K <dankovacek@gmail.com>
2020-01-18 16:43:57 -0800

    add histogram and selection functionality

---
+++ bkapps/flood_msmt/figures.py
@@ -0,0 +1,146 @@
+import numpy as np
+import pandas as pd
+
+from bokeh.plotting import figure
+from bokeh.models import Band 
+
+def create_vhist(peak_source, p):
+    # create the vertical histogram
+    LINE_ARGS = dict(color="#3A5785", line_color=None)
+
+    data = peak_source.data['PEAK']
+    vhist, vedges = np.histogram(data, bins='auto')
+    vzeros = np.zeros(len(vedges)-1)
+    vmax = max(vhist)*1.1
+
+    pv = figure(toolbar_location=None, plot_width=150, plot_height=p.height, x_range=(0, vmax),
+            y_range=p.y_range, min_border=10, y_axis_location="right")
+    pv.ygrid.grid_line_color = None
+    pv.xaxis.major_label_orientation = np.pi/4
+    pv.background_fill_color = "#fafafa"
+
+    pv.quad(left=0, bottom=vedges[:-1], top=vedges[1:], right=vhist, 
+            color="blue", alpha=0.25, line_color="#3A5785")
+    vh1 = pv.quad(left=0, bottom=vedges[:-1], top=vedges[1:], right=vzeros, alpha=0.5, **LINE_ARGS)
+    vh2 = pv.quad(left=0, bottom=vedges[:-1], top=vedges[1:], right=vzeros, alpha=0.1, **LINE_ARGS)
+    
+    return vedges, vzeros, vh1, vh2, pv
+
+def create_ts_plot(peak_source, peak_sim_source, peak_flagged_source):
+    ts_plot = figure(title="Annual Maximum Flood",
+                    width=700,
+                    height=250,
+                    output_backend="webgl",
+                    tools='box_select,lasso_select,reset,box_zoom,pan')
+
+    ts_plot.xaxis.axis_label = "Year"
+    ts_plot.yaxis.axis_label = "Flow [m³/s]"
+
+
+    # add the simulated measurement error points
+    ts_plot.triangle('YEAR', 'PEAK', source=peak_sim_source, 
+                    legend_label='Sim. Msmt. Error', size=3, 
+                    color='red', alpha=0.5)
+
+    # add the recorded measurement values
+    ts_plot.circle('YEAR', 'PEAK', source=peak_source, legend_label="Measured Data", size=4)
+    ts_plot.circle('YEAR', 'PEAK', source=peak_flagged_source, color="orange",
+                legend_label="(QA/QC Flag)", size=4)
+
+
+    # ts_plot.line('YEAR', 'Mean', source=peak_source, color='red',
+    #             legend_label='Mean Annual Max', line_dash='dashed')
+
+    ts_plot.legend.location = "top_left"
+    ts_plot.legend.click_policy = 'hide'
+    return ts_plot
+
+def create_ffa_plot(peak_source, peak_sim_source, peak_flagged_source, 
+                    distribution_source, sim_distribution_source):
+    # create a plot for the Flood Frequency Values and style its properties
+    ffa_plot = figure(title="Flood Frequency Analysis Explorer",
+                    x_range=(0.9, 2E2),
+                    x_axis_type='log',
+                    width=800,
+                    height=500,
+                    output_backend="webgl")
+
+    ffa_plot.xaxis.axis_label = "Return Period (Years)"
+    ffa_plot.yaxis.axis_label = "Flow (m³/s)"
+
+    # add the simulated measurement error points
+    ffa_plot.triangle('Tr', 'PEAK', source=peak_sim_source, 
+                    legend_label='Sim. Msmt. Error', size=3, 
+                    color='red', alpha=0.5)
+
+    ffa_plot.circle('Tr', 'PEAK', source=peak_source, legend_label="Measured Data")
+    ffa_plot.circle('Tr', 'PEAK', source=peak_flagged_source, color="orange",
+                    legend_label="Measured Data (QA/QC Flag)")
+    ffa_plot.line('Tr', 'lp3_quantiles_theoretical', color='blue',
+                source=distribution_source,
+                legend_label='Log-Pearson3 (Measured Data)')
+
+    ffa_plot.line('Tr', 'mean', color='navy',
+                line_dash='dotted',
+                source=distribution_source,
+                legend_label='Mean Simulation')
+
+    ffa_plot.line('Tr', 'expected_value', color='green',
+                source=distribution_source,
+                legend_label='Expected Value')
+
+    ffa_plot.line('Tr', 'lp3_quantiles_theoretical', color='red',
+                line_dash='dotted',
+                source=sim_distribution_source,
+                legend_label='Simulated Error Fit')
+
+    # plot the error bands as shaded areas
+    ffa_2_sigma_band = Band(base='Tr', lower='lower_2s_bound', upper='upper_2s_bound', level='underlay',
+                            fill_alpha=0.25, fill_color='#1c9099',
+                            source=distribution_source)
+    ffa_1_sigma_band = Band(base='Tr', lower='lower_1s_bound', upper='upper_1s_bound', level='underlay',
+                            fill_alpha=0.65, fill_color='#a6bddb', 
+                            source=distribution_source)
+
+    ffa_plot.add_layout(ffa_2_sigma_band)
+    ffa_plot.add_layout(ffa_1_sigma_band)
+
+    ffa_plot.legend.location = "top_left"
+    ffa_plot.legend.click_policy = "hide"
+
+    return ffa_plot
+
+
+def create_qq_plot(peak_source):    
+    # prepare a Q-Q plot
+    qq_plot = figure(title="Q-Q Plot",
+                    width=400,
+                    height=300,
+                    output_backend="webgl")
+
+    qq_plot.xaxis.axis_label = "Empirical Flow [m³/s]"
+    qq_plot.yaxis.axis_label = "Theoretical Flow [m³/s]"
+
+    qq_plot.circle('PEAK', 'lp3_quantiles_theoretical', source=peak_source)
+    qq_plot.line('PEAK', 'PEAK', source=peak_source, legend_label='1:1',
+                color='green')
+
+    qq_plot.legend.location = 'top_left'
+    return qq_plot
+
+def create_pp_plot(peak_source):
+    # prepare a P-P plot
+    pp_plot = figure(title="P-P Plot",
+                    width=400,
+                    height=300,
+                    output_backend="webgl")
+
+    pp_plot.xaxis.axis_label = "Empirical P(x)"
+    pp_plot.yaxis.axis_label = "Theoretical P(x)"
+
+    pp_plot.circle('empirical_cdf', 'theoretical_cdf', source=peak_source)
+    pp_plot.line('empirical_cdf', 'empirical_cdf', source=peak_source, legend_label='1:1',
+                color='green')
+
+    pp_plot.legend.location = 'top_left'
+    return pp_plot
--- bkapps/flood_msmt/main.py
+++ bkapps/flood_msmt/main.py
@@ -18,6 +18,9 @@ from bokeh.layouts import row, column
 from bokeh.models import CustomJS, Slider, Band, Spinner
 from bokeh.plotting import figure, curdoc, ColumnDataSource
 from bokeh.models.widgets import AutocompleteInput, Div, Toggle
+from bokeh.models.widgets import DataTable, TableColumn
+
+from figures import *
 
 path = os.path.abspath(os.path.dirname(__file__))
 if not path in sys.path:
@@ -55,15 +58,9 @@ def set_up_model(df):
     log_skew = st.skew(np.log10(df['PEAK']))
     model = pd.DataFrame()
     model['Tr'] = np.linspace(1.01, 200, 500)
-    model.set_index('Tr', inplace=True)
-    model['z'] = list(map(norm_ppf, model.index.values))
-
-    lp3_calc = 2 / log_skew * \
-        (np.power((model['z'] - log_skew / 6) * log_skew / 6 + 1, 3) - 1)
-
-    model['lp3_quantiles_model'] = np.power(10, np.mean(
-        np.log10(df['PEAK'])) + lp3_calc*np.std(np.log10(df['PEAK'])))
-
+    model['z'] = list(map(norm_ppf, model['Tr']))
+    model.set_index('Tr', inplace=True)    
+    model['lp3_quantiles_theoretical'] = LP3_calc(df['PEAK'], model['z'])
     return model
 
 
@@ -76,13 +73,13 @@ def randomize_msmt_err(val):
 def log_mapper(val):
     return math.log(val)
 
-def LP3_calc(row, z):
+def LP3_calc(data, z_array):
     # calculate the log-pearson III distribution
-    log_skew = st.skew(np.log10(row))
+    log_skew = st.skew(np.log10(data))
     lp3 = 2 / log_skew * \
-        (np.power((z - log_skew / 6) * log_skew / 6 + 1, 3) - 1)
+        (np.power((z_array - log_skew / 6) * log_skew / 6 + 1, 3) - 1)
     lp3_model = np.power(10, np.mean(
-        np.log10(row)) + lp3 * np.std(np.log10(row)))
+        np.log10(data)) + lp3 * np.std(np.log10(data)))
     return lp3_model
 
 def fit_LP3(df):
@@ -91,20 +88,17 @@ def fit_LP3(df):
     # This will be our basis of comparison
     # plot the log-pearson fit to the entire dataset
 
-    z_model = np.array(list(map(norm_ppf, (len(df) + 1) / df['rank'])))
+    z_theoretical = np.array(list(map(norm_ppf, (len(df) + 1) / df['rank'])))
     z_empirical = np.array(list(map(norm_ppf, df['Tr'])))
 
-    lp3_model = LP3_calc(df['PEAK'], z_model)
-    lp3_empirical = LP3_calc(df['PEAK'], z_empirical)
+    df['lp3_quantiles_theoretical'] = LP3_calc(df['PEAK'], z_theoretical)
+    df['lp3_quantiles_empirical'] = LP3_calc(df['PEAK'], z_empirical)
 
-    df['lp3_quantiles_model'] = np.power(10, np.mean(
-        np.log10(df['PEAK'])) + lp3_model*np.std(np.log10(df['PEAK'])))
-
-    df['lp3_quantiles_empirical'] = np.power(10, np.mean(
-        np.log10(df['PEAK'])) + lp3_empirical*np.std(np.log10(df['PEAK'])))
+    print(df[['lp3_quantiles_empirical', 'lp3_quantiles_theoretical']])
 
     # reverse the order for proper plotting on P-P plot
-    df['theoretical_cdf'] = st.pearson3.cdf(z_empirical, skew=st.skew(np.log10(df['PEAK'])))[::-1]
+    log_skew = st.skew(np.log10(df['PEAK'].values.flatten()))
+    df['theoretical_cdf'] = st.pearson3.cdf(z_empirical, skew=log_skew)
 
     # need to remember why I've added 1 to the denominator
     # has to do with sample vs. population (degrees of freedom)?
@@ -154,12 +148,6 @@ def run_ffa_simulation(data, n_simulations):
 
     vectorized_random_value_matrix = v_func(simulation_matrix)
 
-    # sample_set = data.sample(
-    #     sample_size_input.value, replace=False)
-
-    # For the measurement uncertainty simulation, use all points
-    # instead of sampling from sample subset
-
     data = calculate_Tr(peak_values, years, flags)
 
     model = set_up_model(data)
@@ -172,7 +160,7 @@ def run_ffa_simulation(data, n_simulations):
 
     lp3_model = np.apply_along_axis(LP3_calc, 1,
                                     vectorized_random_value_matrix,
-                                    z=model['z'].values.flatten())
+                                    z_array=model['z'].values.flatten())
 
     model['lower_1s_bound'] = np.apply_along_axis(calculate_percentile, 0, lp3_model, p=33.333)
     model['upper_1s_bound'] = np.apply_along_axis(calculate_percentile, 0, lp3_model, p=66.667)
@@ -183,12 +171,17 @@ def run_ffa_simulation(data, n_simulations):
 
     return model
 
+def calculate_sample_statistics(x):
+    pass
+    
 
 def get_data_and_initialize_dataframe():
     station_name = station_name_input.value.split(':')[-1].strip()
 
     df = get_annual_inst_peaks(
         NAMES_TO_IDS[station_name])
+
+    stats = calculate_sample_statistics(df['PEAK'].values.flatten())
 
     if len(df) < 2:
         error_info.text = "Error, insufficient data in record (n = {}).  Resetting to default.".format(
@@ -229,10 +222,12 @@ def update():
     time0 = time.time()
     model = run_ffa_simulation(df, n_simulations)
     time_end = time.time()
+
     print("Time for {:.0f} simulations = {:0.2f} s".format(
         n_simulations, time_end - time0))
 
-    # update the peak flow data source   
+    df = df.sort_values('PEAK')
+    # update the data sources  
     peak_source.data = peak_source.from_df(df)
     peak_sim_source.data = peak_sim_source.from_df(df)
     data_flag_filter = df[~df['SYMBOL'].isin([None, ' '])]
@@ -262,6 +257,7 @@ def update_msmt_error(attr, old, new):
         msmt_error_input.value = 1
         error_info.text = "Minimum measurement error is 0."
     update()
+    update_sim()
 
 
 def update_simulation_sample_size(attr, old, new):
@@ -283,6 +279,20 @@ def update_sim(foo):
     peak_sim_source.data = data
     sim_distribution_source.data = model
 
+def update_pv(attr, old, new):
+    data = peak_source.data['PEAK']
+    inds = new
+    if len(inds) == 0 or len(inds) == len(data):
+        vhist1, vhist2 = vzeros, vzeros
+    else:
+        neg_inds = np.ones_like(data, dtype=np.bool)
+        neg_inds[inds] = False
+        vhist1, _ = np.histogram(data[inds], bins=vedges)
+        vhist2, _ = np.histogram(data[neg_inds], bins=vedges)
+
+    vh1.data_source.data["right"] =  vhist1
+    vh2.data_source.data["right"] = -vhist2
+
 # configure Bokeh Inputs, data sources, and plots
 autocomplete_station_names = list(STATIONS_DF['Station Name'])
 peak_source = ColumnDataSource(data=dict())
@@ -291,6 +301,7 @@ peak_sim_source = ColumnDataSource(data=dict())
 distribution_source = ColumnDataSource(data=dict())
 sim_distribution_source = ColumnDataSource(data=dict())
 qq_source = ColumnDataSource(data=dict())
+datatable_source = ColumnDataSource(data=dict())
 
 
 station_name_input = AutocompleteInput(
@@ -316,6 +327,15 @@ ffa_info = Div(
 
 error_info = Div(text="", style={'color': 'red'})
 
+
+# Set up data table for summary statistics
+columns = [
+    TableColumn(field="parameter", title="Parameter"),
+    TableColumn(field="value", title="Value"),
+    TableColumn(field="units", title="Units"),
+]
+data_table = DataTable(source=datatable_source, columns=columns, width=400, height=280, index_position=None)
+
 # callback for updating the plot based on a changes to inputs
 station_name_input.on_change('value', update_station)
 simulation_number_input.on_change('value', update_n_simulations)
@@ -331,122 +351,28 @@ update()
 update_sim(1)
 
 # widgets
-ts_plot = figure(title="Annual Maximum Flood",
-                width=800,
-                height=250,
-                output_backend="webgl")
+ts_plot = create_ts_plot(peak_source, peak_sim_source, peak_flagged_source)
 
-ts_plot.xaxis.axis_label = "Year"
-ts_plot.yaxis.axis_label = "Flow [m³/s]"
+vedges, vzeros, vh1, vh2, pv = create_vhist(peak_source, ts_plot)
 
+peak_source.selected.on_change('indices', update_pv)
 
-# add the simulated measurement error points
-ts_plot.triangle('YEAR', 'PEAK', source=peak_sim_source, 
-                legend_label='Sim. Msmt. Error', size=3, 
-                color='red', alpha=0.5)
+ffa_plot = create_ffa_plot(peak_source, peak_sim_source, peak_flagged_source,
+                            distribution_source, sim_distribution_source)
 
-# add the recorded measurement values
-ts_plot.circle('YEAR', 'PEAK', source=peak_source, legend_label="Measured Data", size=4)
-ts_plot.circle('YEAR', 'PEAK', source=peak_flagged_source, color="orange",
-            legend_label="(QA/QC Flag)", size=4)
+qq_plot = create_qq_plot(peak_source)
 
-
-# ts_plot.line('YEAR', 'Mean', source=peak_source, color='red',
-#             legend_label='Mean Annual Max', line_dash='dashed')
-
-ts_plot.legend.location = "top_left"
-ts_plot.legend.click_policy = 'hide'
-
-# create a plot for the Flood Frequency Values and style its properties
-ffa_plot = figure(title="Flood Frequency Analysis Explorer",
-                x_range=(0.9, 2E2),
-                x_axis_type='log',
-                width=800,
-                height=500,
-                output_backend="webgl")
-
-ffa_plot.xaxis.axis_label = "Return Period (Years)"
-ffa_plot.yaxis.axis_label = "Flow (m³/s)"
-
-# add the simulated measurement error points
-ffa_plot.triangle('Tr', 'PEAK', source=peak_sim_source, 
-                legend_label='Sim. Msmt. Error', size=3, 
-                color='red', alpha=0.5)
-
-ffa_plot.circle('Tr', 'PEAK', source=peak_source, legend_label="Measured Data")
-ffa_plot.circle('Tr', 'PEAK', source=peak_flagged_source, color="orange",
-                legend_label="Measured Data (QA/QC Flag)")
-ffa_plot.line('Tr', 'lp3_quantiles_model', color='blue',
-            source=distribution_source,
-            legend_label='Log-Pearson3 (Measured Data)')
-
-ffa_plot.line('Tr', 'mean', color='navy',
-            line_dash='dotted',
-            source=distribution_source,
-            legend_label='Mean Simulation')
-
-ffa_plot.line('Tr', 'expected_value', color='green',
-            source=distribution_source,
-            legend_label='Expected Value')
-
-ffa_plot.line('Tr', 'lp3_quantiles_model', color='red',
-            line_dash='dotted',
-            source=sim_distribution_source,
-            legend_label='Simulated Error Fit')
-
-# plot the error bands as shaded areas
-ffa_2_sigma_band = Band(base='Tr', lower='lower_2s_bound', upper='upper_2s_bound', level='underlay',
-                        fill_alpha=0.25, fill_color='#1c9099',
-                        source=distribution_source)
-ffa_1_sigma_band = Band(base='Tr', lower='lower_1s_bound', upper='upper_1s_bound', level='underlay',
-                        fill_alpha=0.65, fill_color='#a6bddb', 
-                        source=distribution_source)
-
-ffa_plot.add_layout(ffa_2_sigma_band)
-ffa_plot.add_layout(ffa_1_sigma_band)
-
-ffa_plot.legend.location = "top_left"
-ffa_plot.legend.click_policy = "hide"
-
-# prepare a Q-Q plot
-qq_plot = figure(title="Q-Q Plot",
-                width=400,
-                height=300,
-                output_backend="webgl")
-
-qq_plot.xaxis.axis_label = "Empirical Flow [m³/s]"
-qq_plot.yaxis.axis_label = "Theoretical Flow [m³/s]"
-
-qq_plot.circle('PEAK', 'lp3_quantiles_theoretical', source=peak_source)
-qq_plot.line('PEAK', 'PEAK', source=peak_source, legend_label='1:1',
-            line_dash='dashed', color='green')
-
-qq_plot.legend.location = 'top_left'
-
-# prepare a P-P plot
-pp_plot = figure(title="P-P Plot",
-                width=400,
-                height=300,
-                output_backend="webgl")
-
-pp_plot.xaxis.axis_label = "Empirical P(x)"
-pp_plot.yaxis.axis_label = "Theoretical P(x)"
-
-pp_plot.circle('empirical_cdf', 'theoretical_cdf', source=peak_source)
-pp_plot.line('empirical_cdf', 'empirical_cdf', source=peak_source, legend_label='1:1',
-            line_dash='dashed', color='green')
-
-pp_plot.legend.location = 'top_left'
+pp_plot = create_pp_plot(peak_source)
 
 # create a page layout
 layout = column(station_name_input,
                 # sample_size_input,
                 row(simulation_number_input, msmt_error_input, toggle_button),
                 error_info,
-                ts_plot,
+                row(ts_plot, pv),
                 ffa_info,
                 ffa_plot,
-                # row(qq_plot, pp_plot)
+                row(qq_plot, pp_plot)
                 )
 
 curdoc().add_root(layout)
